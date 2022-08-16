8f87f9e1fe3f270dabf3d855e16127b3a6e31c66
qhdwight <qhdwight@gmail.com>
2022-08-15 18:23:48 -0700

    Add Bevy project

--- examples/robot.rs
+++ examples/robot.rs
@@ -1,4 +1,22 @@
+use bevy::prelude::*;
 
 fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(WindowDescriptor {
+            title: String::from("Robot Simulator"),
+            ..default()
+        })
+        .add_startup_system(setup)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
 
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("branding/icon.png"),
+        transform: Transform::from_xyz(100.0, 0.0, 0.0),
+        ..default()
+    });
 }
